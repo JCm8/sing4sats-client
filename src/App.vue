d4324ef8d22979e15d23fba5b4209329fa6d9df3
jaonoctus <jaonoctus@protonmail.com>
2024-10-28 19:59:42 -0300

    feat: add tailwindcss and chadcn

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
 
+function onClick() {
+  alert('it works')
+}
 </script>
 
 <template>
   <main>
+    <Button @click.prevent="onClick">test</Button>
   </main>
 </template>
